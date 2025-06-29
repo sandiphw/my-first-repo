c729680d5c84ca153457af0cd776d7f0815d9be6
Sandip <sndiphw@hotmail.com>
2025-06-29 20:32:45 +0530

    Adding python file nltk2

---
+++ nltk2.py
@@ -0,0 +1,4 @@
+import nltk
+from nltk.tokenize import sent_tokenize
+text = "Let us understand the difference between sentence & word tokenizer. It is going to be a simple example."
+print(sent_tokenize(text))
